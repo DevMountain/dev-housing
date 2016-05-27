83db2193be96a829ad078ad9c6686891296f14eb
Jered North <jered@thenorths.net>
2016-05-27 11:55:01 -0600

    updated file structure and routes

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,10 +1,18 @@
 angular.module("devHousing").config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
+
+    $urlRouterProvider.otherwise('/home');
+
     .state('home', {
       url: '/home',
       templateUrl: '/components/home/homeView.html',
       controller: 'homeCtrl'
-    });
+    })
 
-    $urlRouterProvider.otherwise('/home');
+    .state('work-orders', {
+      url: '/work-orders',
+      templateUrl: '/components/home/work-ordersView.html',
+      controller: 'homeCtrl'
+    })
+
 });
