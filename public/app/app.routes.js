96953f6388766a0b5e53d95ec976382c490805a9
Jered North <jered@thenorths.net>
2016-05-30 18:24:24 -0600

    updated side bar with highlited selections and admin Routes

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -19,6 +19,63 @@ angular.module("devHousing").config(function($stateProvider, $urlRouterProvider)
       controller: 'workordersCtrl'
     })
 
+    //ADMIN ROUTES ============================
+
+    .state('admin-workorders', {
+      url: '/admin/workorders',
+      templateUrl: './app/components/admin/workorder/workorder.html',
+      // controller: 'workorderCtrl' //conflicts with other workorder stuff???
+    })
+
+    .state('admin-currentHousing', {
+      url: '/admin/currenthousing',
+      templateUrl: './app/components/admin/currentHousing/currentHousing.html',
+      // controller: 'currentHousingCtrl'
+    })
+
+    .state('admin-futureHousing', {
+      url: '/admin/futurehousing',
+      templateUrl: './app/components/admin/futureHousing/futureHousing.html',
+      // controller: 'futureHousingCtrl'
+    })
+
+    .state('admin-rent', {
+      url: '/admin/rent',
+      templateUrl: './app/components/admin/rent/rent.html',
+      // controller: 'rentCtrl'
+    })
+
+    .state('admin-addEditUser', {
+      url: '/admin/addedituser',
+      templateUrl: './app/components/admin/addEditUser/addEditUser.html',
+      // controller: 'addEditUserCtrl'
+    })
+
+    .state('admin-options', {
+      url: '/admin/options',
+      templateUrl: './app/components/admin/options/options.html',
+      // controller: 'optionsCtrl'
+    })
+
+    .state('admin-check-in', {
+      url: '/admin/check-in',
+      templateUrl: './app/components/admin/check-in/check-in.html',
+      // controller: 'check-inCtrl'
+    })
+
+    .state('admin-check-out', {
+      url: '/admin/check-out',
+      templateUrl: './app/components/admin/check-out/check-out.html',
+      // controller: 'check-outCtrl'
+    })
+
+    .state('admin-home', {
+      url: '/admin/home',
+      templateUrl: './app/components/admin/adminHome/adminHome.html',
+      // controller: 'adminHomeCtrl'
+    })
+
+
     $urlRouterProvider.otherwise('/login');
 
 });
