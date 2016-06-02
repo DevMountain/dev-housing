61c09a3833cff572bda60dad30739f32ae74655a
Matthew Nicholas <phatcatmatt@gmail.com>
2016-06-02 13:47:23 -0600

    updated user schema

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -3,30 +3,47 @@ var bcrypt = require('bcryptjs')
 var Schema = mongoose.Schema;
 
 var User = new Schema ({
+  //required properties
   firstName: {type: String, required: true, trim: true},
   lastName: {type: String, required: true, trim: true},
   birthdate: {type: Date, required: true}, //convert to date first
   gender: {type: String, required: true},
-  devmtnID: {type: Number},
-  campus: {type: String},
   email: {type: String, index: true, trim: true, required: true},
   password: {type: String, required: true},
-  cohortID: [{type: Number}],
-  role: {type: String, required: true, default: 'student'}, //default
-  adminAccess: [{type: String}],
-  adminDefaultView: [{type: String}],
-  adminNotes: {type: String},
   phone: {type: String, required: true},
-  licenseOnFile: {type: Boolean},
-  backgroundCheck: {type: Boolean},
-  rent: {
-    isRenter: {type: Boolean, default: false},
-    price: {type: Number, default: 0},
-    paid: [{type: Date}]
-  },
+  inHousing:{type: Boolean, required: true, default: false},
+  campus: {type: String, required: true},
   currentAddress: {
     city: {type: String, required: true},
     state: {type: String, required: true}
+  },
+  role: {type: String, required: true, default: 'student'},
+  //not required
+  devmtnID: {type: Number},
+  cohortID: [{type: String}],
+  adminAccess: [{type: String}],
+  adminDefaultView: [{type: String}],
+  adminNotes: {type: String},
+  licenseOnFile: {type: Boolean},
+  backgroundCheck: {type: Boolean},
+  deposit: {
+    depositAmount: {type: Number},
+    depositPaid: {type: Date},
+    depositReturned: {type: Date}
+  },
+  singleRoom: {
+    wantsSingleRoom: {type: Boolean, defualt: false},
+    hasSingleRoom: {type: Boolean, default: false},
+    singleRoomFee: {
+      roomFeePaid: {type: Boolean, default: false},
+      amount: {type: Number},
+      paidDate: {type: Date}
+    }
+  },
+  rent: {
+    isRenter: {type: Boolean, default: false},
+    rentPrice: {type: Number, default: 0},
+    rentPaid: [{type: Date}]
   },
   car: {
     make: {type: String},
@@ -34,16 +51,11 @@ var User = new Schema ({
     year: {type: Number},
     license: {type: String}
   },
-  deposit: {
-    amount: {type: Number},
-    paid: {type: Date},
-    returned: {type: Date}
-  },
   checkInOut: {
     checkInDate: {type: Date},
     checkInStatus: {type: Boolean},
     checkOutDate: {type: Date},
-    checkOutStatus: {type: Boolean},
+    checkOutStatus: {type: Boolean}
   }
 })
 
