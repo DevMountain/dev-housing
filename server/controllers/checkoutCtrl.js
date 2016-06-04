05fca6f19e762534a8cd56707f1e12bc75d6df21
Johnathan Lund <Johnathan Lund>
2016-06-04 15:31:00 -0600

    Fixed student checkin/out to only see by cohort id, and admin can see all regardless.

--- server/controllers/checkoutCtrl.js
+++ server/controllers/checkoutCtrl.js
@@ -11,13 +11,26 @@ module.exports = {
   },
 
   read: function(req, res, next) {
-    Checkout.find(req.query, function (err, response) {
-      if(err) { res.status(500).send(err)
-      } else {
-        console.log("Reading Checkout.");
-        res.status(200).send(response);
-      }
-    });
+      Checkout.find(req.query, function(err, response) {
+          if (req.user.role === 'admin') {
+              if (err) {
+                  res.status(500).send(err)
+              } else {
+                  res.status(200).send(response);
+              };
+          };
+      });
+      if (req.user.role === 'student') {
+          req.query.cohort = req.user.cohortID;
+          Checkout.find(req.query).populate("checkinAppointments").exec(function(err, response) {
+              if (err) {
+                  res.status(500).send(err)
+              } else {
+                  console.log("Found right Cohort to show");
+                  res.status(200).json(response);
+              }
+          });
+      };
   },
 
   readById: function(req, res, next) {
