358fba5e936f27ca868d4e518f933511954c344a
Johnathan Lund <johnathanlund@users.noreply.github.com>
2016-06-07 21:52:19 -0600

    Update WorkorderModel.js

--- server/models/WorkorderModel.js
+++ server/models/WorkorderModel.js
@@ -6,7 +6,7 @@ problemType: {type: String, required: true},
 description: {type: String, required: true},
 dateSubmitted: {type: String, required: true, default: new Date()},
 adminNotes: {type: String},
-status: {type: String, required: true, default: 'Received'},
+status: {type: String, required: true, default: 'Pending'},
 submittedBy: {type: Schema.Types.ObjectId, ref: 'User'},
 unit: {type: Schema.Types.ObjectId, ref: 'Unit'}//,
 // room: {type: String, required: true}
