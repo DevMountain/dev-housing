17865ebda487f711af40a0d27d63179ecc91164f
Jered North <jered@thenorths.net>
2016-06-09 09:39:28 -0600

    mobile side bar

--- public/app/shared/studentSideBar/studentSideBarDirective.js
+++ public/app/shared/studentSideBar/studentSideBarDirective.js
@@ -1,6 +1,20 @@
 angular.module('devHousing').directive('studentSideBarNav', function() {
 	return {
 		restrict: "E",
-		templateUrl: './app/shared/studentSideBar/studentSideBar.html'
+		templateUrl: './app/shared/studentSideBar/studentSideBar.html',
+		link: function(scope, elem, attrs) {
+
+			$(".student-side-bar-slide-switch").click(function() {
+				$(".student-side-bar-container").css({
+					transform: 'translateX(0px)'
+				});
+			});
+
+			$(".student-side-bar-logo").click(function() {
+				$(".student-side-bar-container").css({
+					transform: 'translateX(-266px)'
+				});
+			});
+		}
 	};
 });
