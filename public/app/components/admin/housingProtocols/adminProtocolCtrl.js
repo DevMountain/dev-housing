6d7d22ac287a4e2cf4add1271d2ca569d095d0b0
Johnathan Lund <jalshield@gmail.com>
2016-06-10 11:23:27 -0600

    Working on the front end, controller and service set up.

---
+++ public/app/components/admin/housingProtocols/adminProtocolCtrl.js
@@ -0,0 +1,46 @@
+angular.module('devHousing').controller('adminProtocolCtrl', function($scope, protocolSvc, user) {
+
+$scope.user = user;
+
+// Selecting a Housing Protocol to edit/remove
+$scope.selectProtocol = function(protocol) {
+  $scope.selected = protocol;
+};
+
+// Add new Housing Protocol
+$scope.newProtocol = {};
+$scope.addNewProtocol = function(newProtocol) {
+  protocolSvc.createProtocol(newProtocol).then(function(response) {
+    $scope.displayAllProtocols();
+    $scope.newProtocol = {};
+  });
+};
+
+// Display all Housing protocols
+$scope.displayAllProtocols = function() {
+  protocolSvc.getProtocols().then(function(response) {
+    $scope.protocol = response;
+  });
+};
+
+$scope.displayAllProtocols();
+
+//  Update sing Housing Protocol
+$scope.selected = {};
+$scope.updateProtocol = function(protocol) {
+  protocolSvc.updateProtocol(protocol).then(function(response) {
+    $scope.displayAllProtocols();
+    $scope.selected = {};
+  });
+};
+
+//  Delete single Housing Protocol
+$scope.selected = {};
+$scope.deleteProtocol = function(protocol) {
+  protocolSvc.deleteProtocol(protocol).then(function(response) {
+    $scope.displayAllProtocols();
+    $scope.selected = {};
+  });
+};
+
+});
