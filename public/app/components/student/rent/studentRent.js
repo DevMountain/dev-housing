14fb9e20c58738b58af15306a649fe90f7490be6
TheGrimStever <senior.baca@gmail.com>
2016-06-16 00:41:50 -0600

    fixed student rent view by formatting the date of rent paid

--- public/app/components/student/rent/studentRent.js
+++ public/app/components/student/rent/studentRent.js
@@ -3,5 +3,15 @@ angular.module('devHousing').controller('studentRentCtrl', function($scope, user
     // User info
     $scope.user = user;
 
+    var rentPaid = function() {
+      var rentPaidConverted = [];
+      for (var j = 0; j < $scope.user.rent.rentPaid.length; j++){
+        var rentDate = moment($scope.user.rent.rentPaid[j]).format('M/D/YY');
+        rentPaidConverted.push(rentDate);
+      }
+    $scope.user.rent.rentPaidConverted = rentPaidConverted;
+}
+
+  rentPaid();
 
 });  // closing tag
