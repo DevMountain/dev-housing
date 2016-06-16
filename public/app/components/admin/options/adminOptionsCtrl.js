a9b600fe9dc745a1a0f47d8335f741baf5b0652b
Johnathan Lund <jalshield@gmail.com>
2016-06-15 18:48:39 -0600

    fixed bug on admin options, for dallas juniors

--- public/app/components/admin/options/adminOptionsCtrl.js
+++ public/app/components/admin/options/adminOptionsCtrl.js
@@ -22,18 +22,21 @@ angular.module('devHousing').controller('adminOptionsCtrl', function($scope, coh
     //show save button to edit cohorts
     $scope.editProvo = false;
     $scope.edit_Provo = function() {
+        $('.provo_input').prop("disabled", false);
         $scope.editProvo = !$scope.editProvo;
     };
 
 
     $scope.editSLC = false;
     $scope.edit_SLC = function() {
+        $('.slc_input').prop("disabled", false);
         $scope.editSLC = !$scope.editSLC;
     };
 
 
     $scope.editDallas = false;
     $scope.edit_Dallas = function() {
+        $('.dallas_input').prop("disabled", false);
         $scope.editDallas = !$scope.editDallas;
     };
 
