1de244a25d55d0c2e3559587981ff7c3f5e0f871
Johnathan Lund <jalshield@gmail.com>
2016-06-15 23:33:49 -0600

    commit before master merge

--- public/app/components/admin/cohorts/adminCohortsCtrl.js
+++ public/app/components/admin/cohorts/adminCohortsCtrl.js
@@ -5,25 +5,25 @@ angular.module('devHousing').controller('adminCohortsCtrl', function($scope, coh
     //get all cohorts on page load
     var callCohorts = function() {
         cohortSvc.getCohorts().then(function(response) {
-          $scope.provoSenior = angular.copy(response.Provo.senior);
-          $scope.provoJunior = angular.copy(response.Provo.junior);
-          $scope.provoFuture = angular.copy(response.Provo.future);
-          $scope.slcSenior = angular.copy(response.SLC.senior);
-          $scope.slcJunior = angular.copy(response.SLC.junior);
-          $scope.slcFuture = angular.copy(response.SLC.future);
-          $scope.dallasSenior = angular.copy(response.Dallas.senior);
-          $scope.dallasJunior = angular.copy(response.Dallas.junior);
-          $scope.dallasFuture = angular.copy(response.Dallas.future);
-          $scope.allCohorts = response;
+            $scope.provoSenior = angular.copy(response.Provo.senior);
+            $scope.provoJunior = angular.copy(response.Provo.junior);
+            $scope.provoFuture = angular.copy(response.Provo.future);
+            $scope.slcSenior = angular.copy(response.SLC.senior);
+            $scope.slcJunior = angular.copy(response.SLC.junior);
+            $scope.slcFuture = angular.copy(response.SLC.future);
+            $scope.dallasSenior = angular.copy(response.Dallas.senior);
+            $scope.dallasJunior = angular.copy(response.Dallas.junior);
+            $scope.dallasFuture = angular.copy(response.Dallas.future);
+            $scope.allCohorts = response;
         });
     };
     callCohorts();
 
     //save updated cohorts
-    $scope.saveCohorts = function(cohorts){
-      cohortSvc.saveCohorts(cohorts).then(function(response){
-        //add saved indicator
-    });
+    $scope.saveCohorts = function(cohorts) {
+        cohortSvc.saveCohorts(cohorts).then(function(response) {
+            //add saved indicator
+        });
     };
 
 });
