a8402272ad4d184e6cc5ee912a747daa2924385d
Johnathan Lund <jalshield@gmail.com>
2016-06-15 23:31:22 -0600

    fixed two way binding on Cohorts when editing cohort id

--- public/app/components/admin/cohorts/adminCohortsCtrl.js
+++ public/app/components/admin/cohorts/adminCohortsCtrl.js
@@ -5,11 +5,19 @@ angular.module('devHousing').controller('adminCohortsCtrl', function($scope, coh
     //get all cohorts on page load
     var callCohorts = function() {
         cohortSvc.getCohorts().then(function(response) {
-            $scope.allCohorts = response;
+          $scope.provoSenior = angular.copy(response.Provo.senior);
+          $scope.provoJunior = angular.copy(response.Provo.junior);
+          $scope.provoFuture = angular.copy(response.Provo.future);
+          $scope.slcSenior = angular.copy(response.SLC.senior);
+          $scope.slcJunior = angular.copy(response.SLC.junior);
+          $scope.slcFuture = angular.copy(response.SLC.future);
+          $scope.dallasSenior = angular.copy(response.Dallas.senior);
+          $scope.dallasJunior = angular.copy(response.Dallas.junior);
+          $scope.dallasFuture = angular.copy(response.Dallas.future);
+          $scope.allCohorts = response;
         });
     };
     callCohorts();
-
 
     //save updated cohorts
     $scope.saveCohorts = function(cohorts){
